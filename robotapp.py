4153407425e7169b87aeca6313b69d98773dd5ee
Rystic <chris.bielak@gmail.com>
2015-10-02 17:56:23 -0400

    Updated to use Memcache.
    Removed code that queried GAE's database, and replaced it with memcache.

--- robotapp.py
+++ robotapp.py
@@ -6,35 +6,18 @@ import datetime
 from google.appengine.ext import webapp
 from google.appengine.ext import ndb
 from google.appengine.api import users
-
-guestbook_key = ndb.Key('Guestbook', 'default_guestbook')
-
-class Greeting(ndb.Model):
-  author = ndb.UserProperty()
-  content = ndb.TextProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
+from google.appengine.api import memcache
 
 class MainPage(webapp2.RequestHandler):
 
-    content = ndb.TextProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    
     def get(self):
         self.response.out.write('<html><body>')
-        
-        greetings = ndb.gql('SELECT * '
-                        'FROM Greeting '
-                        'WHERE ANCESTOR IS :1 '
-                        'ORDER BY date DESC LIMIT 1',
-                        guestbook_key)
 
-        for greeting in greetings:
-              self.response.out.write(greeting.content)
-
+        self.response.out.write(memcache.get('dir'))
         self.response.out.write("""
-          <form action="/sign" method="post">
+          <form action="/memcacheput" method="post">
             <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Post instruction (w, a, s, d)"></div>
+            <div><input type="submit" value="Post instruction (w, a, s, d) "></div>
           </form>
         </body>
       </html>""")
@@ -45,18 +28,12 @@ class MainPage(webapp2.RequestHandler):
         for line in f:
             self.response.out.write(line)
 
-class Guestbook(webapp2.RequestHandler):
+class MemCachePut(webapp2.RequestHandler):
     def post(self):
-        greeting = Greeting(parent=guestbook_key)
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-
-        greeting.content = self.request.get('content')
-        greeting.put()
+        memcache.set(key='dir', value=self.request.get('content'))
         self.redirect('/')        
 
 app = webapp2.WSGIApplication([
   ('/', MainPage),
-  ('/sign', Guestbook)
+  ('/memcacheput', MemCachePut)
 ], debug=True)
