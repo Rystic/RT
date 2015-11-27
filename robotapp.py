5748b4b25b054cde55ad14ad2de5c3cf069231cc
Rystic <chris.bielak@gmail.com>
2015-11-27 17:02:01 -0500

    Updated RT to listen for key presses.
    RT now listens for W A S D instead of taking input from a text area.

--- robotapp.py
+++ robotapp.py
@@ -16,8 +16,56 @@ class MainPage(webapp2.RequestHandler):
         self.response.out.write(memcache.get('dir'))
         self.response.out.write("""
           <form action="/memcacheput" method="post">
-            <div><textarea name="content" rows="3" cols="60"></textarea></div>
-            <div><input type="submit" value="Post instruction (w, a, s, d) "></div>
+            <script>
+                var prevChar = null;
+
+                function click(e) {
+                   
+                    var charCode = String.fromCharCode(e.keyCode);
+                    sendKey(charCode);
+                };
+
+                function release(e)
+                {
+                    sendKey(null);
+                };
+
+                function sendKey(charCode)
+                {
+                    if (prevChar == charCode)
+                    {
+                        return;
+                    }
+                    prevChar = charCode;
+                    console.log(charCode);
+                    post('/memcacheput', {value: charCode});
+                }
+                
+                function post(path, params, method) {
+                    method = method || "post"; // Set method to post by default if not specified.
+
+                    // The rest of this code assumes you are not using a library.
+                    // It can be made less wordy if you use one.
+                    var form = document.createElement("form");
+                    form.setAttribute("method", method);
+                    form.setAttribute("action", path);
+
+                    for(var key in params) {
+                        if(params.hasOwnProperty(key)) {
+                            var hiddenField = document.createElement("input");
+                            hiddenField.setAttribute("type", "hidden");
+                            hiddenField.setAttribute("name", key);
+                            hiddenField.setAttribute("value", params[key]);
+
+                            form.appendChild(hiddenField);
+                         }
+                    }
+                    document.body.appendChild(form);
+                    form.submit();
+                };
+                document.onkeypress = click;
+                document.onkeyup = release;
+            </script>
           </form>
         </body>
       </html>""")
@@ -30,7 +78,7 @@ class MainPage(webapp2.RequestHandler):
 
 class MemCachePut(webapp2.RequestHandler):
     def post(self):
-        memcache.set(key='dir', value=self.request.get('content'))
+        memcache.set(key='dir', value=self.request.get('value'))
         self.redirect('/')        
 
 app = webapp2.WSGIApplication([
