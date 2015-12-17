edb339a628f150ef880faf684923ef47899fd94a
Rystic <chris.bielak@gmail.com>
2015-12-17 12:37:43 -0500

    Updated website to use Javascript and Ajax.
    Website will no longer reload the page when pressing keys.

--- robotapp.py
+++ robotapp.py
@@ -1,87 +1,36 @@
-import sys
 import webapp2
-import cgi
-import urllib
-import datetime
-from google.appengine.ext import webapp
-from google.appengine.ext import ndb
-from google.appengine.api import users
+import os
+
+from google.appengine.ext.webapp.template import render
 from google.appengine.api import memcache
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        self.response.out.write('<html><body>')
+        tmpl = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.write(render(tmpl, {'value' : memcache.get('d1')}))
 
-        self.response.out.write(memcache.get('dir'))
-        self.response.out.write("""
-          <form action="/memcacheput" method="post">
-            <script>
-                var prevChar = null;
-
-                function click(e) {
-                   
-                    var charCode = String.fromCharCode(e.keyCode);
-                    sendKey(charCode);
-                };
-
-                function release(e)
-                {
-                    sendKey(null);
-                };
-
-                function sendKey(charCode)
-                {
-                    if (prevChar == charCode)
-                    {
-                        return;
-                    }
-                    prevChar = charCode;
-                    console.log(charCode);
-                    post('/memcacheput', {value: charCode});
-                }
-                
-                function post(path, params, method) {
-                    method = method || "post"; // Set method to post by default if not specified.
-
-                    // The rest of this code assumes you are not using a library.
-                    // It can be made less wordy if you use one.
-                    var form = document.createElement("form");
-                    form.setAttribute("method", method);
-                    form.setAttribute("action", path);
-
-                    for(var key in params) {
-                        if(params.hasOwnProperty(key)) {
-                            var hiddenField = document.createElement("input");
-                            hiddenField.setAttribute("type", "hidden");
-                            hiddenField.setAttribute("name", key);
-                            hiddenField.setAttribute("value", params[key]);
-
-                            form.appendChild(hiddenField);
-                         }
-                    }
-                    document.body.appendChild(form);
-                    form.submit();
-                };
-                document.onkeypress = click;
-                document.onkeyup = release;
-            </script>
-          </form>
-        </body>
-      </html>""")
-
+class RTPage(webapp2.RequestHandler):
+    def get(self):
+        value = memcache.get('d1')
+        self.response.out.write("""<html><body><p>""")
+        self.response.out.write(value)
+        self.response.out.write("""</p></body></html>""");
+        
     def post(self):
-        url = 'http://robotapp-1041.appspot.com'
-        f = urllib.urlopen(url)
-        for line in f:
-            self.response.out.write(line)
+        memcache.set(key='d1', value=self.request.get('value'))
 
-class MemCachePut(webapp2.RequestHandler):
+class AnthonyBotPage(webapp2.RequestHandler):
+    def get(self):
+        value = memcache.get('d2')
+        self.response.out.write("""<html><body><p>""")
+        self.response.out.write(value)
+        self.response.out.write("""</p></body></html>""");
+        
     def post(self):
-        memcache.set(key='dir', value=self.request.get('value'))
-        self.redirect('/')        
+        memcache.set(key='d2', value=self.request.get('value'))
 
 app = webapp2.WSGIApplication([
-  ('/', MainPage),
-  ('/memcacheput', MemCachePut)
+    ('/', MainPage),
+    ('/rt', RTPage),
+    ('/anthonybot', AnthonyBotPage),
 ], debug=True)
