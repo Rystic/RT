3b81311f2b1ecb925212c40752a2027515b3fcbd
Rystic <chris.bielak@gmail.com>
2015-12-18 21:58:00 -0500

    (#3) Re-register after disconnect.
    After a disconnect, a rover will attempt to re-register itself as active
    before polling for commands.

--- piCommandInterpreter.py
+++ piCommandInterpreter.py
@@ -6,26 +6,29 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
-#establish connection
-
-
 if len(sys.argv) < 2:
     print 'Error: No name provided.'
     sys.exit()
 name = sys.argv[1]
 print 'Name:', str(name)
 
-r = requests.post("http://robotapp-1041.appspot.com/register", data={'name' : name})
+registered = 0
 
-while 1:
+if 1:
     try:
-        aResp = urllib.urlopen("http://robotapp-1041.appspot.com/rt");
-        web_pg = aResp.read();
-        instruction = web_pg[15:19]
-        if (instruction != 'None'):
-            hexInstruction = hex(int(instruction, 2))[2]
-            ser.write(hexInstruction)
-        requests.post("http://robotapp-1041.appspot.com/heartbeat", data={'name' : name})
+        if not registered:
+            requests.post("http://robotapp-1041.appspot.com/register", data={'name' : name})
+            registered = 1
+            print "registered " + name
+        else:
+            aResp = urllib.urlopen("http://robotapp-1041.appspot.com/rt");
+            web_pg = aResp.read();
+            instruction = web_pg[15:19]
+            if (instruction != 'None'):
+                hexInstruction = hex(int(instruction, 2))[2]
+                ser.write(hexInstruction)
+            requests.post("http://robotapp-1041.appspot.com/heartbeat", data={'name' : name})
         time.sleep(.1)
     except IOError:
-        time.sleep(5)
+        time.sleep(10)
+        registered = 0
