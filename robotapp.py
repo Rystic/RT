fa4926efc75431d586a8373269e0707ccef55a0c
Rystic <chris.bielak@gmail.com>
2015-12-18 20:57:52 -0500

    (#1) New page to register active robots.
    Active robots are put into the ndb.

--- robotapp.py
+++ robotapp.py
@@ -3,41 +3,39 @@ import os
 
 from google.appengine.ext.webapp.template import render
 from google.appengine.api import memcache
+from google.appengine.ext import ndb
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         tmpl = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.write(render(tmpl, {'value' : memcache.get('d1')}))
 
-class RTPage(webapp2.RequestHandler):
-    def get(self):
-        value = memcache.get('d1')
-        self.response.out.write("""<html><body><p>""")
-        self.response.out.write(value)
-        self.response.out.write("""</p></body></html>""");
-        
-    def post(self):
-        memcache.set(key='d1', value=self.request.get('value'))
-
-class AnthonyBotPage(webapp2.RequestHandler):
-    def get(self):
-        value = memcache.get('d2')
-        self.response.out.write("""<html><body><p>""")
-        self.response.out.write(value)
-        self.response.out.write("""</p></body></html>""");
-        
-    def post(self):
-        memcache.set(key='d2', value=self.request.get('value'))
-
 class RegisterRobotPage(webapp2.RequestHandler):
     def get(self):
-        name = memcache.get('name')
+        self.response.out.write("""<html><body><p>""")
+        self.response.out.write(getActiveRobots())
+        self.response.out.write("""</p></body></html>""")
         
     def post(self):
-        name=self.request.get('name')
+        active_robots = getActiveRobots()
+        name = self.request.get('name')
+        if active_robots.count(name) == 0:
+            robot = RobotEntity()
+            robot.name = name
+            robot_key = robot.put()
+
+class RobotEntity(ndb.Model):
+    name = ndb.StringProperty()
+
+def getActiveRobots():
+    active_robots = [];
+    qry = RobotEntity.query()
+    for key in qry.iter(keys_only=True):
+        active_robots.append(key.get().name)
+    return active_robots
+    
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/rt', RTPage),
-    ('/anthonybot', AnthonyBotPage),
+    ('/register', RegisterRobotPage),
 ], debug=True)
