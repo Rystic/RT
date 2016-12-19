df3b5d41be7eb3cc764d858113667fc03df5f334
Rystic <chris.bielak@gmail.com>
2016-12-18 21:52:15 -0500

    Minor Updates
    Nothing of note, but needs to be committed.

--- pi python/simulated_robot.py
+++ pi python/simulated_robot.py
@@ -1,7 +1,10 @@
 from readers.wasd_reader import WASDReader
 from writers.simulated_writer import SimulatedWriter
 import robot_runner
+import random
+
+names = ['Alpha', 'Curiosity 0', 'Wall-E', 'Voltron', 'Dalek', 'R.O.B.', 'Bioroid', 'Eli', 'Viktor']
+
+nameIndex = random.randrange(0, len(names))
 
 robot_runner.execute(WASDReader("rt"), SimulatedWriter())
-
-# generate a random name
